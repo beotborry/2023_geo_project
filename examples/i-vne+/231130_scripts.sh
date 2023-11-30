39a6064183609bbc8827393968fb1a39fd5daa59
juhyeon <parkjh9229@naver.com>
2023-11-30 17:13:17 +0900

    fix scripts

--- examples/i-vne+/231130_scripts.sh
+++ examples/i-vne+/231130_scripts.sh
@@ -1,9 +1,9 @@
-python -W ignore -u train_ivne_cifar10.py --gpu_num 0,1,2,3 --datadir ./data/ --cache_name 231130_Cifar_10_vne --seed 0 &
-python -W ignore -u train_ivne_cifar10.py --gpu_num 0,1,2,3 --datadir ./data/ --cache_name 231130_Cifar_10_frobenius --reg_type frobenius --seed 0 &
+python -W ignore -u train_ivne_cifar10.py --gpu_num 0,1,2,3 --datadir ./data/ --cache_name 231130_Cifar_10_vne --seed 0 --batch_size 128 & 
+#python -W ignore -u train_ivne_cifar10.py --gpu_num 0,1,2,3 --datadir ./data/ --cache_name 231130_Cifar_10_frobenius --reg_type frobenius --seed 0 --batch_size 128 & wait
 python -W ignore -u train_ivne_cifar10.py --gpu_num 0,1,2,3 --datadir ./data/ --cache_name 231130_Cifar_10_geodesic --reg_type geodesic --seed 0 & wait
 
 python -W ignore -u eval_ivne_cifar10.py --gpu_num 0 --datadir ./data/ --cache_name 231130_Cifar_10_vne --seed 0 &
-python -W ignore -u eval_ivne_cifar10.py --gpu_num 0 --datadir ./data/ --cache_name 231130_Cifar_10_frobenius --reg_type frobenius --seed 0 &
+#python -W ignore -u eval_ivne_cifar10.py --gpu_num 0 --datadir ./data/ --cache_name 231130_Cifar_10_frobenius --reg_type frobenius --seed 0 & wait
 python -W ignore -u eval_ivne_cifar10.py --gpu_num 0 --datadir ./data/ --cache_name 231130_Cifar_10_geodesic --reg_type geodesic --seed 0 & wait
 
 
--- examples/i-vne+/231130_scripts_imagenet.sh
+++ examples/i-vne+/231130_scripts_imagenet.sh
@@ -1,5 +1,5 @@
-python -W ignore -u train_ivne_imagenet100.py --gpu_num 0,1,2,3 --datadir ./data/imagenet --cache_name 231130_ImageNet_100 --seed 0 --reg_type geodesic --batch_size 128 &
-python -W ignore -u train_ivne_imagenet100.py --gpu_num 0,1,2,3 --datadir ./data/imagenet --cache_name 231130_ImageNet_100 --seed 0 --reg_type vne --batch_size 128 & wait
+python -W ignore -u train_ivne_imagenet100.py --gpu_num 0,1,2,3 --datadir /home/juhyeon/Imagenet --cache_name 231201_ImageNet_10_geo --seed 0 --reg_type geodesic --batch_size 128 &
+python -W ignore -u train_ivne_imagenet100.py --gpu_num 0,1,2,3 --datadir /home/juhyeon/Imagenet --cache_name 231201_ImageNet_10_vne --seed 0 --reg_type vne --batch_size 128 & wait
 
-python -W ignore -u eval_ivne_imagenet100.py --gpu_num 0 --datadir ./data/imagenet --cache_name 231130_ImageNet_100 --seed 0 --reg_type geodesic &
-python -W ignore -u eval_ivne_imagenet100.py --gpu_num 0 --datadir ./data/imagenet --cache_name 231130_ImageNet_100 --seed 0 --reg_type vne & wait
+python -W ignore -u eval_ivne_imagenet100.py --gpu_num 0 --datadir /home/juhyeon/Imagenet --cache_name 231201_ImageNet_10_geo --seed 0 --reg_type geodesic &
+python -W ignore -u eval_ivne_imagenet100.py --gpu_num 0 --datadir /home/juhyeon/Imagenet --cache_name 231201_ImageNet_10_vne --seed 0 --reg_type vne & wait
